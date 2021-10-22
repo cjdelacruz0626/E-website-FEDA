51cdd4d5a5a12198ecaa0c8a34cd5e4c84901199
De La Cruz <c.o.dela.cruz@accenture.com>
2021-10-22 15:08:36 +0800

    Finalized the Navbar, created js for toggle menu

---
+++ BasicKleta/script.js
@@ -0,0 +1,9 @@
+
+// toggle menu bar 
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+} 
