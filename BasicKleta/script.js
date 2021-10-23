35f3ea36c431378e2411eea4ef9628fc93c960ec
De La Cruz <c.o.dela.cruz@accenture.com>
2021-10-24 01:43:19 +0800

    Finalized displaying data

--- BasicKleta/script.js
+++ BasicKleta/script.js
@@ -66,3 +66,83 @@ const repeat = function (activeClass) {
     repeater();
 }
 repeat();
+
+
+
+//Add to cart functionalities
+//variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-btn');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+//Cart
+let cart = [];
+
+// Getting the products
+class Products {
+    async getProduct() {
+        try {
+            let result = fetch('products.json');
+            let data = await (await result).json();
+            let products = data.items;
+
+            // iterate through the item
+            // deconstruct the item
+            products = products.map(item => {
+                //ES6 Destructuring 
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+
+                // different due to contenful
+                const image = item.fields.image.fields.file.url;
+
+                return { title, price, id, image };
+            });
+
+            return products;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+// display products
+class UI {
+    displayProducts(products) {
+        // console.log(products);
+        let result = "";
+        products.forEach(product => {
+            result +=
+                ` 
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product-1" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}><i class="fas fa-shopping-cart"></i>add to cart</button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>₱${product.price.toLocaleString()}</h4>
+            </article> 
+            `;
+        });
+        productsDOM.innerHTML = result;
+    }
+}
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+
+    // get all products
+    products.getProduct().then(products => ui.displayProducts(products));
+})
